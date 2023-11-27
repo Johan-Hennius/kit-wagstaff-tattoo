5b0b1e51e2022bef0b99defe68dff1d8b8f36ed0
Johan Hennius <johan.bh19@gmail.com>
2023-11-27 09:04:46 +0000

    Initial commit

---
+++ kit_wagstaff_tattoo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for kit_wagstaff_tattoo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kit_wagstaff_tattoo.settings')
+
+application = get_asgi_application()
