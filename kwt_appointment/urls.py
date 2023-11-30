e90df0809e4aed7b0e70227715096eaad83c7d07
Johan Hennius <johan.bh19@gmail.com>
2023-11-30 15:41:40 +0000

    Approved and pending bookings added to my_bookings.html. Edit functionality creaed for both booking types

--- kwt_appointment/urls.py
+++ kwt_appointment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateBooking, my_bookings
+from .views import CreateBooking, my_bookings, UpdateBooking
 
 urlpatterns = [
     path("", CreateBooking.as_view(), name="booking_form"),
     path("my-bookings/", my_bookings, name="my_bookings"),
+    path("update-booking/<slug:pk>", UpdateBooking.as_view(), name="update_booking"),
 ]
--- kwt_appointment/views.py
+++ kwt_appointment/views.py
@@ -26,10 +26,28 @@ class CreateBooking(LoginRequiredMixin, CreateView):
 
 
 def my_bookings(request):
+    user_profile = request.user
+    approved_bookings = Booking.objects.filter(email_address = request.user, confirmed=True)
+    pending_bookings = Booking.objects.filter(email_address = request.user, confirmed=False)
+    
     return render(
-        request,
+        request, 
         'bookings/my_bookings.html',
         {
-
+            'approved_bookings': approved_bookings,
+            'pending_bookings': pending_bookings,
         }
     )
+
+
+class UpdateBooking(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    Update a booking
+    """
+    template_name = 'bookings/update_booking.html'
+    model = Booking
+    form_class = BookingForm
+    success_url = '/booking/my-bookings/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().email_address
