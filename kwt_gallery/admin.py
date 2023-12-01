1122b347873230465828256e591d95b826ceb6a5
Johan Hennius <johan.bh19@gmail.com>
2023-12-01 16:00:41 +0000

    Register gallery model

--- kwt_gallery/admin.py
+++ kwt_gallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Gallery
 
-# Register your models here.
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+
+    list_display = "title", "slug", "created_on"
+    search_fields = ["title"]
+    list_filter = ("title", "created_on",)
+    prepopulated_fields = {"slug": ("title",)}
