28fea7c047906cd01e356ba932ea1822b5b60536
Johan Hennius <johan.bh19@gmail.com>
2023-12-01 12:47:34 +0000

    Update url path for delete function

--- kwt_appointment/urls.py
+++ kwt_appointment/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import my_bookings, update_booking, DeleteBooking, create_booking
+from .views import my_bookings, update_booking, delete_booking, create_booking
 
 urlpatterns = [
     path("", create_booking, name="booking_form"),
     path("my-bookings/", my_bookings, name="my_bookings"),
-    path("delete-booking/<slug:pk>", DeleteBooking.as_view(), name="delete_booking"),
+    path("delete-booking/<int:booking_id>", delete_booking, name="delete_booking"),
     path("update-booking/<int:booking_id>", update_booking, name="update_booking"),
 ]
