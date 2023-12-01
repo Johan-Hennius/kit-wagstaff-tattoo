2acdecca6e54b987af7fbe5b336fb73759707946
Johan Hennius <johan.bh19@gmail.com>
2023-12-01 10:57:47 +0000

    Adjust url path for new booking function

--- kwt_appointment/urls.py
+++ kwt_appointment/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import my_bookings, UpdateBooking, DeleteBooking, create_booking
+from .views import my_bookings, update_booking, DeleteBooking, create_booking
 
 urlpatterns = [
     path("", create_booking, name="booking_form"),
     path("my-bookings/", my_bookings, name="my_bookings"),
     path("delete-booking/<slug:pk>", DeleteBooking.as_view(), name="delete_booking"),
-    path("update-booking/<slug:pk>", UpdateBooking.as_view(), name="update_booking"),
+    path("update-booking/<int:booking_id>", update_booking, name="update_booking"),
 ]
