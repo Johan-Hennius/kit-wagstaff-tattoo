86324321b76bb82f9370024e17571504c7df9ece
Johan Hennius <johan.bh19@gmail.com>
2023-12-01 16:00:07 +0000

    Create gallery model

--- kwt_gallery/models.py
+++ kwt_gallery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from cloudinary.models import CloudinaryField
+
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    gallery_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Title: {self.title} | Uploaded: {self.created_on}"
