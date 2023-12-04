c150b8f932c48aad20981a086ef539270b471cb4
Johan Hennius <johan.bh19@gmail.com>
2023-12-04 10:35:37 +0000

    Clean up main url.py

--- kwt_misc/urls.py
+++ kwt_misc/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("tattoo-care/", views.tattoo_care, name="tattoo_care"),
     path("terms/", views.terms, name="terms"),
-    path("contact/", views.contact, name="contact"),
 ]
